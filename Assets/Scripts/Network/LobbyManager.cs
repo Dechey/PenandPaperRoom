675b473fb00330000826cf5071518c947aa4a7a8
Kai Friese <okorian@web.de>
2020-12-08 23:40:08 +0100

    Voice Nr 1

---
+++ Assets/Scripts/Network/LobbyManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private GameObject createRoomButton;
+    [SerializeField]
+    private GameObject joinRoomButton;
+    [SerializeField]
+    private int roomSize;
+    public Text roomName;
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        createRoomButton.SetActive(true);
+        joinRoomButton.SetActive(true);
+    }
+
+    public void CreateRoom()
+    {
+        Debug.Log("Creating new room");
+        createRoomButton.SetActive(false);
+        joinRoomButton.SetActive(false);
+
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)roomSize };
+        PhotonNetwork.CreateRoom(roomName.text, roomOps);
+        Debug.Log("New room created" + roomName.text);
+    }
+
+    public void JoinRoom()
+    {
+        Debug.Log("Joining room now");
+        createRoomButton.SetActive(false);
+        joinRoomButton.SetActive(false);
+
+        PhotonNetwork.JoinRoom(roomName.text);
+        Debug.Log("Joined to " + roomName.text);
+    }
+}
---
+++ Assets/Scripts/Network/RoomSetupManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class RoomSetupManager : MonoBehaviour
+{
+    public void Start()
+    {
+        CreateUser();
+    }
+
+    private void CreateUser()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            Debug.Log("Creating User");
+            PhotonNetwork.Instantiate(Path.Combine("Prefabs", "User"), Vector3.zero, Quaternion.identity);
+        }
+    }
+}
